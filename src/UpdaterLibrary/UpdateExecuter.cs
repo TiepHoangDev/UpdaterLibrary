15a5ddfd54e2f16ef8a5d6016f10a142c45a2a41
tiephoangdev <tiephoang.dev@gmail.com>
2024-01-04 14:35:33 +0700

    add Fody

--- src/UpdaterLibrary/UpdateExecuter.cs
+++ src/UpdaterLibrary/UpdateExecuter.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
-using System.Linq;
 using System.Net.Http;
 using System.Reflection;
-using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
-using System.Xml;
 
 namespace UpdaterLibrary
 {
